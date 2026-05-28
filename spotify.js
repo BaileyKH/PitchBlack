58d678900cae5010821badeec89ea9f6daa1daf0
Bailey Henderson <hen.baileyk@gmail.com>
2026-05-28 09:48:09 -0700

    feat: add playlist items and start plackback functions for listening functionality

--- spotify.js
+++ spotify.js
@@ -1,3 +1,4 @@
+import { start } from "node:repl"
 
 
 async function getPlaylists(access_token) {
@@ -14,6 +15,40 @@ async function getPlaylists(access_token) {
     return data
 }
 
+async function getPlaylistItems(access_token, playlistId) {
+    const headers = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + access_token
+        }
+    }
+
+    const res = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/items`, headers)
+    const data = await res.json()
+
+    return data
+}
+
+async function startPlayback(access_token, uri) {
+    const headers = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + access_token
+        },
+        body: JSON.stringify({
+            context_uri: uri
+        })
+    }
+
+    const res = await fetch(`https://api.spotify.com/v1/me/player/play`, headers)
+    const data = await res.json()
+
+    return data
+}
+
 module.exports = {
-    getPlaylists
+    getPlaylists,
+    getPlaylistItems,
+    startPlayback
 }
